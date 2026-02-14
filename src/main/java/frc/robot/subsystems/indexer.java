e151f29f459766b3976667c038a13adff395bc44
Sj <20semajjackson10@gmail>
2026-02-14 16:20:02 -0500

    Set up indexer motor configs

--- src/main/java/frc/robot/subsystems/indexer.java
+++ src/main/java/frc/robot/subsystems/indexer.java
@@ -4,7 +4,12 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -13,10 +18,40 @@ public class indexer extends SubsystemBase {
   /** Creates a new indexer. */
   private TalonFX indexerMotor = new TalonFX(Constants.indexerConstants.indexerMotorID);
   
-  public indexer() {}
+  public indexer() {
+    applyIndexerMotorConfigs();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void applyIndexerMotorConfigs(){
+    TalonFXConfiguration talonconfigs = new TalonFXConfiguration();
+
+    talonconfigs.Slot0.kP = Constants.indexerConstants.kP;
+    talonconfigs.Slot0.kI = Constants.indexerConstants.kI;
+    talonconfigs.Slot0.kD = Constants.indexerConstants.kD;
+    talonconfigs.Slot0.kV = Constants.indexerConstants.kv;
+    talonconfigs.Slot0.kS = Constants.indexerConstants.ks;
+    talonconfigs.Slot0.kA = Constants.indexerConstants.ka;
+
+    var motionMagicConfigs = talonconfigs.MotionMagic;
+    motionMagicConfigs.MotionMagicCruiseVelocity = Constants.indexerConstants.MotionMagicCruiseVelocity;
+    motionMagicConfigs.MotionMagicAcceleration = Constants.indexerConstants.MotionMagicAcceleration;
+    motionMagicConfigs.MotionMagicJerk = Constants.indexerConstants.MotionMagicJerk;
+
+    talonconfigs.Feedback.FeedbackRemoteSensorID = indexerMotor.getDeviceID();
+    talonconfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+    
+    indexerMotor.getConfigurator().apply(talonconfigs);
+
+    MotorOutputConfigs motorOutputConfigs = talonconfigs.MotorOutput;
+    motorOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
+    motorOutputConfigs.NeutralMode = NeutralModeValue.Brake;
+
+    indexerMotor.getConfigurator().apply(motorOutputConfigs);
+
   }
 }

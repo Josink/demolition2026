3bf6460e4ca101246d041b2449ebd0e53945d317
Jay Sinkaiye <139293681+Josink@users.noreply.github.com>
2026-02-27 10:27:05 -0500

    Initialize data logging in Robot constructor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,8 @@ import com.ctre.phoenix6.HootAutoReplay;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -21,6 +23,8 @@ public class Robot extends TimedRobot {
         .withJoystickReplay();
 
     public Robot() {
+        DataLogManager.start();
+        DriverStation.startDataLog(DataLogManager.getLog());
         m_robotContainer = new RobotContainer();
     }
 
